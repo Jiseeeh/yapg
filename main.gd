8edd652f49f9a054f3ae4a0fd658d8f958cf627d
Jiseeeh <johncarlo.camara1@gmail.com>
2023-09-23 20:36:20 +0800

    fix: fix exit on run

--- main.gd
+++ main.gd
@@ -7,14 +7,9 @@ func _ready():
 
 func _on_play_button_pressed():
 	# TODO: move to world_level select
-	get_tree().change_scene_to_file("res://levels/01/WorldOne.tscn")
+	get_tree().change_scene_to_file("res://levels/level_select.tscn")
 
 
 func _on_quit_button_pressed():
 	GameManager.save_game()
 	get_tree().quit()
-
-
-func _notification(what):
-	if what == NOTIFICATION_WM_CLOSE_REQUEST || NOTIFICATION_WM_GO_BACK_REQUEST:
-		_on_play_button_pressed()
