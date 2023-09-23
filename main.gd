b1020db8187b808202b7173eb982613252641bae
Jiseeeh <johncarlo.camara1@gmail.com>
2023-09-23 15:07:20 +0800

    feat: load game on ready

--- main.gd
+++ main.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+func _ready():
+	GameManager.load_game()
 
 func _on_play_button_pressed():
-	# or on to what current level (prolly on save)
+	# TODO: move to world_level select
 	get_tree().change_scene_to_file("res://Levels/WorldOne.tscn")
 
+
 func _on_quit_button_pressed():
+	GameManager.save_game()
 	get_tree().quit()
+
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST || NOTIFICATION_WM_GO_BACK_REQUEST:
+		_on_play_button_pressed()
