830d7a3b086ac43958662819ecc6c44adaf01483
Jiseeeh <johncarlo.camara1@gmail.com>
2023-09-29 16:29:00 +0800

    feat: handle character touch

--- levels/character_select.gd
+++ levels/character_select.gd
@@ -11,3 +11,23 @@ func _ready():
 	mask_dude.play("Idle")
 	pink_man.play("Idle")
 	virtual_guy.play("Idle")
+
+
+func _on_frog_man_button_pressed():
+	GameManager.character = "FrogMan"
+	get_tree().change_scene_to_file("res://levels/level_select.tscn")
+
+
+func _on_mask_dude_button_pressed():
+	GameManager.character = "MaskDude"
+	get_tree().change_scene_to_file("res://levels/level_select.tscn")
+
+
+func _on_pink_man_button_pressed():
+	GameManager.character = "PinkMan"
+	get_tree().change_scene_to_file("res://levels/level_select.tscn")
+
+
+func _on_virtual_guy_button_pressed():
+	GameManager.character = "VirtualGuy"
+	get_tree().change_scene_to_file("res://levels/level_select.tscn")
